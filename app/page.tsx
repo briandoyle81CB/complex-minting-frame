a3630dbc3db85186b838fff12d5ade9fef4993a6
Brian Doyle <brian.doyle@coinbase.com>
2024-01-28 13:40:08 -0500

    Reset for ask

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,26 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 
-export async function generateMetadata(): Promise<Metadata> {
-  const imageUrl = `https://land-sea-and-sky.vercel.app/lss-bw.png?date=${Date.now()}`;
-  return {
+
+const imageUrl = `https://land-sea-and-sky.vercel.app/api/images/lss-bw?date=${Date.now()}`;
+
+const frameMetadata = getFrameMetadata({
+  buttons: ['Mint (Not working)'],
+  image: imageUrl,
+  post_url: 'https://land-sea-and-sky.vercel.app/api/frame',
+});
+
+export const metadata: Metadata = {
+  title: 'Land, Sea, and Sky',
+  description: 'A complex, fully onchain NFT, minted exclusively from a Frame!',
+  openGraph: {
     title: 'Land, Sea, and Sky',
     description: 'A complex, fully onchain NFT, minted exclusively from a Frame!',
-    openGraph: {
-      title: 'Land, Sea, and Sky',
-      description: 'A complex, fully onchain NFT, minted exclusively from a Frame!',
-      images: [imageUrl],
-    },
-    other: {
-      buttons: ['Mint (Not working)'],
-      image: imageUrl,
-      post_url: 'https://land-sea-and-sky.vercel.app/api/frame',
-    },
-  };
+    images: [imageUrl],
+  },
+  other: {
+    ...frameMetadata,
+  },
 };
 
 export default function Page() {
