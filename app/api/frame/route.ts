f502747b4e881da2839c792c7a2194a3890f8c5c
Brian Doyle <brian.doyle@coinbase.com>
2024-01-28 18:14:49 -0500

    Undo route

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -15,7 +15,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress = '';
   try {
     const body: { trustedData?: { messageBytes?: string } } = await req.json();
-    accountAddress = await getFrameAccountAddress(body, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
+    accountAddress = await getFrameAccountAddress(body, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' }) as string;
   } catch (err) {
     console.error(err);
     // For local testing
