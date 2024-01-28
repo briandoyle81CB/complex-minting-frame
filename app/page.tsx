1a01be0f39df86f17f8076ef6e417e9633be0d00
Brian Doyle <brian.doyle@coinbase.com>
2024-01-28 12:52:17 -0500

    Bust frameMetadata cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 
-const frameMetadata = getFrameMetadata({
-  buttons: ['Mint (Not working)'],
-  image: 'https://land-sea-and-sky.vercel.app/lss-bw.png',
-  post_url: `https://land-sea-and-sky.vercel.app/api/frame?date=${Date.now()}`,
-});
 
 const imageUrl = `https://land-sea-and-sky.vercel.app/api/images/lss-bw?date=${Date.now()}`;
+
+const frameMetadata = getFrameMetadata({
+  buttons: ['Mint (Not working)'],
+  image: imageUrl,
+  post_url: 'https://land-sea-and-sky.vercel.app/api/frame',
+});
 
 export const metadata: Metadata = {
   title: 'Land, Sea, and Sky',
